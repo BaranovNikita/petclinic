ad8b97de669830100259165ab32c1cfd249d9edd
Никита Баранов <baranov.nikita@gmail.com>
2020-03-09 21:09:12 +0300

    Id generation in map

--- pet-clinic-web/src/main/java/ru/nbaranov/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/ru/nbaranov/sfgpetclinic/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Test");
         owner1.setLastName("Testov");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Owner");
         owner2.setLastName("Ownerov");
 
@@ -37,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owners loaded...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Igor");
         vet1.setLastName("Nikolaev");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Valery");
         vet2.setLastName("Leont'ev");
 
