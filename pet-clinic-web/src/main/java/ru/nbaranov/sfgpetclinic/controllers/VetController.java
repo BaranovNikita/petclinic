f493ddb13747dfef2956a2c18d4751bfa41313d8
Никита Баранов <baranov.nikita@gmail.com>
2020-03-09 20:55:32 +0300

    add vets list

--- pet-clinic-web/src/main/java/ru/nbaranov/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ru/nbaranov/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package ru.nbaranov.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.nbaranov.sfgpetclinic.services.VetService;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/", "/index", "/index.html", ""})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
